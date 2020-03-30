9fb1c158c0903749c4fb58e70e07e433a0ec3f73
hpl8196 <1322237965@qq.com>
2020-03-30 18:51:37 +0800

    aaa

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+var a = 0;
+var D1 = document.getElementById('D1');
+var D2 = document.getElementById('D2');
+var D3 = document.getElementById('D3');
+var banner = document.getElementById('banner');
+D1.onclick = function (){
+		a=-(1903*0);
+		banner.style.left = a+'px';
+	}
+D2.onclick = function (){
+		a=-(1903*1);
+		banner.style.left = a+'px';
+	}
+D3.onclick = function (){
+		a=-(1903*2);
+		banner.style.left = a+'px';
+	}
